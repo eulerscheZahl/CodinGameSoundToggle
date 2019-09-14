1b2831e1ab8332e4334d01477d57a381087963af
eulerscheZahl <eulerschezahl@yandex.com>
2019-09-14 09:30:59 +0200

    sound toggle demonstration

--- src/main/java/com/codingame/game/Referee.java
+++ src/main/java/com/codingame/game/Referee.java
@@ -5,6 +5,9 @@ import com.codingame.gameengine.core.AbstractPlayer.TimeoutException;
 import com.codingame.gameengine.core.AbstractReferee;
 import com.codingame.gameengine.core.MultiplayerGameManager;
 import com.codingame.gameengine.module.entities.GraphicEntityModule;
+import com.codingame.view.AnimatedEventModule;
+import com.codingame.view.ToggleModule;
+import com.codingame.view.ViewerEvent;
 import com.google.inject.Inject;
 
 public class Referee extends AbstractReferee {
@@ -12,14 +15,21 @@ public class Referee extends AbstractReferee {
     // @Inject private SoloGameManager<Player> gameManager;
     @Inject private MultiplayerGameManager<Player> gameManager;
     @Inject private GraphicEntityModule graphicEntityModule;
+    @Inject private AnimatedEventModule animatedEventModule;
+    @Inject private ToggleModule toggleModule;
 
     @Override
     public void init() {
-        // Initialize your game here.
+        graphicEntityModule.createText("Click the gear in the bottom right corner\nto toggle the sound.\n\nThis won't affect already playing sound\nbut only new sound events").setFillColor(0xbbbbbb)
+        .setX(100).setY(100).setFontSize(40);
     }
 
     @Override
     public void gameTurn(int turn) {
+        ViewerEvent soundEvent = animatedEventModule.createAnimationEvent("Sound", 0.0);
+        soundEvent.params.put("path", "sounds/death.wav");
+        soundEvent.params.put("toggle", "soundToggle");
+
         for (Player player : gameManager.getActivePlayers()) {
             player.sendInputLine("input");
             player.execute();
--- src/main/resources/view/config.js
+++ src/main/resources/view/config.js
@@ -1,6 +1,28 @@
 import { GraphicEntityModule } from './entity-module/GraphicEntityModule.js';
+import { AnimatedEventModule } from './animations/AnimatedEventModule.js';
+import { ToggleModule } from './animations/ToggleModule.js';
 
 // List of viewer modules that you want to use in your game
 export const modules = [
-	GraphicEntityModule
+	GraphicEntityModule,
+	ToggleModule,
+	AnimatedEventModule,
 ];
+
+// The list of toggles displayed in the options of the viewer
+export const options = [
+  ToggleModule.defineToggle({
+    // The name of the toggle
+    // replace "myToggle" by the name of the toggle you want to use
+    toggle: 'soundToggle',
+    // The text displayed over the toggle
+    title: 'SOUND TOGGLE',
+    // The labels for the on/off states of your toggle
+    values: {
+      'SOUND ON': true,
+      'SOUND OFF': false
+    },
+    // Default value of your toggle
+    default: true
+  }),
+]
